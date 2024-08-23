6c7856fc8a1aafd3e654ff2fa63a6e8af815c90e
Ran Erlich <ranerlich@gmail.com>
2024-08-23 10:30:03 +0300

    added post request

--- app.py
+++ app.py
@@ -1,16 +1,35 @@
-from flask import Flask
+from flask import Flask, request
 
 # Create an instance of the Flask class
 app = Flask(__name__)
 
-pet1 = {'id':1, 'name':'Dixie'}
-pet2 = {'id':2, 'name':'Charlie'}
+pet1 = {'id':1, 'name':'Dixie', 'age':5,'image':'https://t4.ftcdn.net/jpg/01/99/00/79/360_F_199007925_NolyRdRrdYqUAGdVZV38P4WX8pYfBaRP.jpg'}
+pet2 = {'id':2, 'name':'Charlie','age':2,'image':'https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg'}
 pets = [pet1, pet2]
-# Define a route for the root URL
-@app.route('/')
+
+# pet list
+@app.route('/pets')
 def pets_list():
     return pets
 
+@app.route('/pets', methods=["POST"])
+def add_pet():
+    new_pet = request.get_json()
+    pets.append(new_pet)
+    return {'result':'added succesfuly'}
+
+     
+
+
+@app.route('/pets/<id>/')
+def single_pet(id):
+    try:
+        for pet in pets:
+                if pet['id'] == int(id):
+                    return pet
+    except:
+        print('error in id')
+    return {'result':'Pet not found'}
 
 
 # Run the app only if this script is executed (not imported)
